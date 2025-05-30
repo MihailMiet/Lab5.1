19ed1c039553ccf1e228dfada2d6bb355f57c60c
Mihail Mihail <studymiet0@outlook.com>
2025-05-30 18:27:02 +0300

    Lab5_v1

---
+++ main.c
@@ -0,0 +1,12 @@
+//Дана целочисленная матрица 14x14.
+//Найти максимум в заштрихованной области.
+#include <stdio.h>
+#include <stdlib.h>
+#include <windows.h>
+int main(void) {
+	SetConsoleCP(1251);
+	SetConsoleOutputCP(1251);
+	srand(time(NULL));
+	int M[14][14], indexdif = 0, lcount = 0, hcount = 0, max = 0, fillflag = 0, num = 1;
+	return 0;
+}
